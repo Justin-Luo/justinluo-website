18524509f444d4eea419e45ae0cf1dfaf57adeb5
Justin-Luo <justin.luo.20@dartmouth.edu>
2018-07-19 15:13:04 -0400

    start of website

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$("#main-logo").click(function() {
+  console.log("help");
+  $('html,body').animate({
+      scrollTop: 0,
+    },
+    'slow');
+});
+
+$("#about").click(function() {
+  console.log("help");
+  $('html,body').animate({
+      scrollTop: $(" #about-section").offset().top - 100
+    },
+    'slow');
+});
+
+$("#portfolio").click(function() {
+  console.log("help");
+  $('html,body').animate({
+      scrollTop: $(" #portfolio-section").offset().top - 100
+    },
+    'slow');
+});
+
+
+
+
+
+// });
