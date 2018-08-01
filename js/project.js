a094d9f659847a46c9a0956c2501c1ac8045a285
Justin-Luo <justin.luo.20@dartmouth.edu>
2018-07-31 22:57:59 -0400

    style changes, added more photos to projects

--- js/project.js
+++ js/project.js
@@ -1,11 +1,26 @@
-window.project = ScrollReveal({
-  reset: true,
-  rotate: {
-    x: 0,
-    y: -10,
-    z: 0
-  },
-});
+window.project = ScrollReveal();
 
-project.reveal('.page-header-content');
-project.reveal('.page-quote-background');
+var bottomReveal = {
+  reset: true,
+  scale: 0.9,
+};
+
+var leftReveal = {
+  reset: true,
+  origin: 'left',
+  scale: 1,
+  distance: '20px',
+  duration: 500,
+};
+
+var rightReveal = {
+  reset: true,
+  origin: 'right',
+  scale: 1,
+  distance: '20px',
+  duration: 500,
+};
+
+project.reveal('.page-back-container', leftReveal);
+project.reveal('.page-header-content', bottomReveal);
+project.reveal('.page-quote-background', bottomReveal);
